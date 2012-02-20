43ad96a32bf262f60651bd5c40a96048706c22da
Jason Garber <jg@jasongarber.com>
2012-02-20 16:55:27 -0500

    Do it in Rails. Asset pipeline is easier

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redsnakephilly2012::Application.config.secret_token = '66a67d591834c7a82c94c574a2fb3c55deea231a4de7d3ea22a5397ba8b6db5de6297943b40888e0778198017a5f3225dbdbd898018c5077f2a46f33ff563f71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Redsnakephilly2012::Application.config.session_store :cookie_store, key: '_redsnakephilly2012_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Redsnakephilly2012::Application.config.session_store :active_record_store
